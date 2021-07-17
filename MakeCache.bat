4742bb3bc09b4d060811b15803c0cf5ef67359ec
thewolfgames <thewolfgames@yahoo.com>
2021-07-17 20:47:20 +0300

    TR

--- MakeCache.bat
+++ MakeCache.bat
@@ -7,7 +7,7 @@ set LOC=%~dp0
 
 dir /B /S /A:-D >> test0.txt
 
-findstr /v "media .bat .exe .mp4 offlinexmb.cache test0.txt" test0.txt > test.txt
+findstr /v "media .bat .exe .mp4 .git .rar offlinexmb.cache test0.txt" test0.txt > test.txt
 del test0.txt
 
 @echo off
